65985caf14e67366c8478fa8939c8e40e19836d7
Miguel Peralta <maikperalta123@gmail.com>
2022-12-19 01:23:49 -0700

    nuevos endpoints añadidos

--- src/app.js
+++ src/app.js
@@ -2,6 +2,10 @@ import express from "express"
 import pagosRoutes from "./routes/pagos.routes.js"
 import enviosRoutes from "./routes/envios.routes.js"
 import provRoutes from "./routes/prov.routes.js"
+import transporteRoutes from "./routes/transporte.routes.js"
+import bancoRoutes from "./routes/banco.routes.js"
+import tiendaOnlineRoutes from "./routes/tiendaOnline.routes.js"
+
 
 const app = express();
 
@@ -10,6 +14,9 @@ app.use(express.json())
 app.use(pagosRoutes)
 app.use(enviosRoutes)
 app.use(provRoutes)
+app.use(transporteRoutes)
+app.use(bancoRoutes)
+app.use(tiendaOnlineRoutes)
 
 app.use((req, res, next) => {
     res.status(404).json({
